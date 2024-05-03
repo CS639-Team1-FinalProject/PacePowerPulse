ea211f8eee8b40ea9fdd0cc4ed2512d8723edb9a
avinash1504 <avinashbhavancheekar@gmail.com>
2024-05-03 09:43:16 -0400

    updated signin page

--- Pace Power Pulse/app/src/main/java/com/example/pacepowerpulse/MainActivity.kt
+++ Pace Power Pulse/app/src/main/java/com/example/pacepowerpulse/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.pacepowerpulse
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pacepowerpulse.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding?= null
+    private lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding?.root)
 //        supportActionBar?.hide()
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        print("current user is " + firebaseAuth.currentUser)
+        Log.d("current user is ", firebaseAuth.currentUser?.email.toString())
+        // Check if the user is signed in
+        if (firebaseAuth.currentUser == null) {
+            // If not signed in, launch SignInActivity
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+           finish() // Finish the MainActivity to prevent going back to it after signing in
+        }
         //val flStartButton : FrameLayout = findViewById(R.id.flStart)
         binding?.flStart?.setOnClickListener {
             val intent=Intent(this,ExerciseActivity::class.java)
@@ -37,11 +50,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+        
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
         binding = null
+        FirebaseAuth.getInstance().signOut()
+//        firebaseAuth.signOut()
+       firebaseAuth.currentUser?.delete()
     }
 }
